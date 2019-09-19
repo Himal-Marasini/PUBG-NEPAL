cb7b8942b0f5b540ea25921a674034b6a6e51d11
Himal <rajshh.07@gmail.com>
2019-09-19 22:21:48 +0545

    Added smtp2go

--- middleware/sendMail.js
+++ middleware/sendMail.js
@@ -4,17 +4,17 @@ module.exports = async function sendMail(userdata) {
 
     // create reusable transporter object using the default SMTP transport
     let transporter = nodemailer.createTransport({
-        host: "smtp.mailtrap.io",
-        port: 2525,
+        host: "mail.smtp2go.com",
+        port: 8025,
         auth: {
-            user: "0a778841b9e317",
-            pass: "b2ba4470006141"
+            user: "pubgnepal",
+            pass: "endsMmo1dWphdjAw"
         }
     });
 
     // send mail with defined transport object
     let info = await transporter.sendMail({
-        from: "contact@pubgNepal.com", // sender address
+        from: "contact@pubgmobilenp.com", // sender address
         to: userdata.emailId, // list of receivers
         subject: "MATCH DETAILS", // Subject line
         text: "Please keep this all detail to YourSelf only", // plain text body
@@ -25,5 +25,3 @@ module.exports = async function sendMail(userdata) {
 
     console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
 };
-
-// module.exports = verifyEmail;
