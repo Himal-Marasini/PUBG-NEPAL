746549cfdc9990b7a50ec2edff8f5e097b5d918b
Himal <rajshh.07@gmail.com>
2019-09-24 22:10:35 +0545

    Minor issue

--- assets/js/registration/formValidation.js
+++ assets/js/registration/formValidation.js
@@ -137,7 +137,6 @@ const controller = ((uiCtrl, validCtrl) => {
         validCtrl.validateInput(inputVal);
 
         if (domVariable.state) {
-
             // This is the User Data
             const inputData = validCtrl.userData;
 
