7dae58d58dfd72ac2dbe6226b74f5623372b4a14
Himanshu <rajshh.07@gmail.com>
2020-07-29 09:24:28 +0545

    Refactored the css code

--- index.js
+++ index.js
@@ -1,64 +1,71 @@
 // In Package.json, Change the start script from "Nodemon index.js" to "Node index.js"
 
-const express = require('express');
+const express = require("express");
 const app = express();
-const ejs = require('ejs');
-const bodyparser = require('body-parser');
-const morgan = require('morgan');
-const compression = require('compression');
-const dotenv = require('dotenv');
+const path = require("path");
+const ejs = require("ejs");
+const bodyparser = require("body-parser");
+const morgan = require("morgan");
+const compression = require("compression");
+const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 
-const cors = require('cors');
-const helmet = require('helmet');
-const rateLimit = require('express-rate-limit');
-const mongoSanitize = require('express-mongo-sanitize');
-const xss = require('xss-clean');
+const cors = require("cors");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
 
-const homepage = require('./routes/homepage');
-const authentication = require('./routes/authentication');
-const registration = require('./routes/pubgForm');
-const admin = require('./routes/admin');
+const homepage = require("./routes/homepage");
+const authentication = require("./routes/authentication");
+const registration = require("./routes/pubgForm");
+const admin = require("./routes/admin");
 
-const db = require('./util/databse');
+const db = require("./util/databse");
 
-const error = require('./controllers/error');
+const error = require("./controllers/error");
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 if (process.env.development === "prod") {
-    // app.use(cors());
-    // app.options('*', cors());
+  // app.use(cors());
+  // app.options('*', cors());
 }
 
 app.use(helmet());
 
 const limit = rateLimit({
-    max: 70,
-    windowMs: 60 * 60 * 1000,
-    message: "Too many request from same IP, Please try again after one hour !!!"
+  max: 70,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many request from same IP, Please try again after one hour !!!",
 });
 
 app.use(limit);
 
 app.use(compression());
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
+
+app.set("views", path.join(__dirname, "views"));
 
 // Middleware for Image and css
-app.use('/assets', express.static('assets'));
-app.use('/styles', express.static('styles'));
+app.use("/assets", express.static("assets"));
+app.use("/styles", express.static("styles"));
 
 // Bodyparser - FOR PARSING FORM
-app.use(bodyparser.urlencoded({
+app.use(
+  bodyparser.urlencoded({
     limit: "1mb",
-    extended: false
-}));
+    extended: false,
+  })
+);
 
 // BODYPARSER - FOR JSON BODY
-app.use(bodyparser.json({
-    limit: "1mb"
-}));
+app.use(
+  bodyparser.json({
+    limit: "1mb",
+  })
+);
 
 app.use(mongoSanitize());
 
@@ -79,32 +86,32 @@ app.use(admin);
 app.use(registration);
 
 app.all("*", function (req, res, next) {
-    const err = new Error("404 !!! PAGE NOT FOUND");
-    err.type = "Page Not Found";
-    err.status = 404;
-    err.subtitle = `THE PAGE YOU ARE LOOKING FOR https://pubgmobilenp.com${req.originalUrl} DOESN'T EXISTS !!`
-    next(err);
+  const err = new Error("404 !!! PAGE NOT FOUND");
+  err.type = "Page Not Found";
+  err.status = 404;
+  err.subtitle = `THE PAGE YOU ARE LOOKING FOR https://pubgmobilenp.com${req.originalUrl} DOESN'T EXISTS !!`;
+  next(err);
 });
 
 app.use(error);
 
 db.then(() => {
-    const server = app.listen(process.env.PORT);
-    console.log(`Server is running at PORT ${process.env.PORT}`);
-    process.on("SIGTERM", () => {
-        console.log("SIGTERM !!!! SHUTTING DOWN SERVER");
-        server.close(() => {
-            console.log('Process Terminate !!')
-        })
+  const server = app.listen(process.env.PORT);
+  console.log(`Server is running at PORT ${process.env.PORT}`);
+  process.on("SIGTERM", () => {
+    console.log("SIGTERM !!!! SHUTTING DOWN SERVER");
+    server.close(() => {
+      console.log("Process Terminate !!");
     });
-}).catch(err => {
-    console.error(err);
-    return res.status(500).render('error.ejs', {
-        status: false,
-        errorType: "Server Down",
-        message: {
-            title: '500 !!! INTERNAL SERVER ERROR',
-            subtitle: `PLEASE TRY AGAIN LATER, WE DIDN'T ANTICIPATE THIS TAKING SO LONG.`
-        }
-    });
+  });
+}).catch((err) => {
+  console.error(err);
+  return res.status(500).render("error.ejs", {
+    status: false,
+    errorType: "Server Down",
+    message: {
+      title: "500 !!! INTERNAL SERVER ERROR",
+      subtitle: `PLEASE TRY AGAIN LATER, WE DIDN'T ANTICIPATE THIS TAKING SO LONG.`,
+    },
+  });
 });
