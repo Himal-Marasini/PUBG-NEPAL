ba7dd78c48339e83d9e17c834198bf1e5fe340b1
Himanshu <rajshh.07@gmail.com>
2020-07-31 23:05:07 +0545

    done

---
+++ model/CreateMatch.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+
+const Schema = new mongoose.Schema({
+    date: {
+        type: String,
+        required: true
+    },
+    time: {
+        type: String,
+        required: true
+    },
+    type: {
+        type: String,
+        required: true,
+        uppercase: true
+    },
+    map: {
+        type: String,
+        required: true,
+        uppercase: true
+    },
+    prize: {
+        type: String,
+        required: true
+    },
+    fee: {
+        type: String,
+        required: true
+    },
+    players: [
+        {
+            type: mongoose.SchemaTypes.ObjectId,
+            ref: 'user'
+        }
+    ]
+    ,
+    device: {
+        type: String,
+        required: true,
+        uppercase: true
+    },
+    isFinished: {
+        type: Boolean,
+        default: false
+    }
+});
+
+module.exports = mongoose.model('match', Schema);
